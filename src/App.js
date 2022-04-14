4571fb4adb1b65ee41f2431db06abdcc692ef3ed
Abideen <Abideenali926@gmail.com>
2022-04-13 23:46:03 -0700

    done

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import logo from './logo.svg';
+import './App.css';
+import Doubleheart from './components/Doubleheart'
+function App() {
+  return (
+    <>
+    <Doubleheart />
+    </>
+  );
+}
+
+export default App;
---
+++ src/components/Doubleheart.jsx
@@ -0,0 +1,52 @@
+import React,{useState, useRef} from 'react'
+import './double.css'
+
+function Doubleheart() {
+  const clickMe = useRef(null)
+  const time = useRef(null)
+  const [click,setClick] = useState(0)
+  const [count,setCount] = useState(0)
+  
+    const handleChange = (e) => {
+      if(click == 0) {
+        setClick(new Date().getTime())
+  
+      }else {
+        if((new Date().getTime() - click) < 400){
+         createHeart(e)
+          setClick(0)
+        }else{
+          setClick(new Date().getTime())
+        }
+      }
+    }
+    
+    const createHeart = (e) => {
+      const heart = document.createElement('i');
+      heart.classList.add('fas');
+      heart.classList.add('fa-heart');
+  
+      const x =e.nativeEvent.clientX;
+      const y = e.nativeEvent.clientY;
+      const leftOffset = e.target.offsetLeft;
+      const topOffset = e.target.offsetTop;
+  
+      const xInside = x - leftOffset;
+      const yInside = y - topOffset;
+      
+      heart.style.top = `${yInside}px`;
+      heart.style.left = `${xInside}px`;
+      clickMe.current.appendChild(heart);
+      setCount(count+1);
+      setTimeout(()=>heart.remove(),5000)
+    }
+  
+    return (
+      <>
+      <h3>Double click on the  image to <i className='fas fa-heart'></i> it</h3>
+      <small>You liked it <span id='times' ref={time}>{count}</span>{count > 1 ? " times" : " time"}</small>
+      <div className="loveMe" ref={clickMe} onClick={(e)=>handleChange(e)}></div>
+  </>
+    )
+  }
+export default Doubleheart
